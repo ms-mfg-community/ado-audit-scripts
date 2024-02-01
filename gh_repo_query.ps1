adfec453a3ce86de8edfea398fd8eded80e07b72
Cody Carlson <codycarlson@microsoft.com>
2024-02-01 15:37:39 -0700

    added new information to output

--- gh_repo_query.ps1
+++ gh_repo_query.ps1
@@ -1,8 +1,12 @@
+# Get the personal access token and organization from environment variables
+$pat = [System.Environment]::GetEnvironmentVariable('PAT')
+$organization = [System.Environment]::GetEnvironmentVariable('ORGANIZATION')
+
 # Define your GitHub Personal Access Token
-$pat = "{PAT}}"
+#$pat = "your_pat_here"
 
 # Define the organization
-$organization = "{ORG}"
+#$organization = "ms-mfg-community"
 
 # Define the API URL
 $apiUrl = "https://api.github.com/orgs/$organization/repos"
@@ -23,9 +27,17 @@ $baseApiUrl = "https://api.github.com/repos"
 $repositories = $response | ForEach-Object {
     # Define the API URL for the workflows directory of the current repository
     $apiUrl = "$baseApiUrl/$($_.owner.login)/$($_.name)/contents/.github/workflows"
+    # Define the API URL for the secrets of the current repository
+    $secretsApiUrl = "$baseApiUrl/$($_.owner.login)/$($_.name)/actions/secrets"
+    # Define the API URL for the environments of the current repository
+    $environmentsApiUrl = "$baseApiUrl/$($_.owner.login)/$($_.name)/environments"
 
     # Initialize the hasActions variable to False
     $hasActions = $false
+    # Initialize the hasSecrets variable to False
+    $hasSecrets = $false
+    # Initialize the environments variable to an empty array
+    $environments = @()
 
     # Make the API request and save the response in a variable
     try {
@@ -38,14 +50,51 @@ $repositories = $response | ForEach-Object {
         # An error occurred, so leave hasActions as False
     }
 
-    # Select the desired properties and add the new property
+    # Make the API request and save the response in a variable
+    try {
+        $secretsResponse = Invoke-RestMethod -Uri $secretsApiUrl -Headers $headers -Method Get -ErrorAction Stop
+
+        # Check if the response contains any secrets
+        $hasSecrets = $secretsResponse.secrets.Count -gt 0
+    }
+    catch {
+        # An error occurred, so leave hasSecrets as False
+    }
+
+    try {
+        $environmentsResponse = Invoke-RestMethod -Uri $environmentsApiUrl -Headers $headers -Method Get -ErrorAction Stop
+
+        # Check if the response contains any environments
+        if ($environmentsResponse.environments.Count -gt 0) {
+            $environments = $environmentsResponse.environments | ForEach-Object { $_.name } -join ', '
+        }
+    }
+    catch {
+        # An error occurred, so leave environments as an empty array
+    }
+
+    # Define the API URL for the teams of the current repository
+    $teamsApiUrl = "$baseApiUrl/$($_.owner.login)/$($_.name)/teams"
+
+    # Make the API request and save the response in a variable
+    $teamsResponse = Invoke-RestMethod -Uri $teamsApiUrl -Headers $headers -Method Get
+    # Create a string that contains the names of the teams and their permissions
+
+
+    # Create a string that contains the names of the teams and their permissions
+    $teams = ($teamsResponse | ForEach-Object { "$($_.name):$($_.permission)" }) -join ', '
+
+    # Select the desired properties and add the new properties
     $_ | Select-Object id,node_id,name,full_name,private,created_at,updated_at,pushed_at,git_url,clone_url,size,has_issues,has_projects,has_downloads,has_wiki,has_pages,has_discussions,forks_count,mirror_url,archived,disabled,open_issues_count,license,allow_forking,is_template,web_commit_signoff_required,topics,visibility,forks,open_issues,watchers,default_branch,permissions,
         @{Name='Advanced_Security'; Expression={$_.security_and_analysis.advanced_security}},
         @{Name='secret_scanning'; Expression={$_.security_and_analysis.secret_scanning}},
         @{Name='secret_scanning_push_protection'; Expression={$_.security_and_analysis.secret_scanning_push_protection}},
         @{Name='dependabot_security_updates'; Expression={$_.security_and_analysis.dependabot_security_updates}},
         @{Name='secret_scanning_validity_checks'; Expression={$_.security_and_analysis.secret_scanning_validity_checks}},
-        @{Name='HasActions'; Expression={$hasActions}}
+        @{Name='HasActions'; Expression={$hasActions}},
+        @{Name='Teams'; Expression={$teams}},
+        @{Name='HasSecrets'; Expression={$hasSecrets}},
+        @{Name='Environments'; Expression={$environments}}
 }
 
 # Define the path to the CSV file in the Documents folder
