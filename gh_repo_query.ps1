af8b4aca5dd08a756a68d16c314655a97e2dce3c
Cody Carlson <codycarlson@microsoft.com>
2024-02-06 11:35:54 -0700

    added support for pagination, additional console write-host info on script actions

--- gh_repo_query.ps1
+++ gh_repo_query.ps1
@@ -19,137 +19,174 @@ $headers = @{
     Authorization = "Basic $base64AuthInfo"
 }
 
-$response = Invoke-RestMethod -Uri $apiUrl -Headers $headers -Method Get
+do {
 
-$baseApiUrl = "https://api.github.com/repos"
+    $response = Invoke-RestMethod -Uri $apiUrl -Headers $headers -Method Get
 
-# The list of repositories is in the response
-$repositories = $response | ForEach-Object {
-    Write-Host "Processing repository $($_.name)..."
-    # Define the API URL for the workflows directory of the current repository
-    $apiUrl = "$baseApiUrl/$($_.owner.login)/$($_.name)/contents/.github/workflows"
-    # Define the API URL for the secrets of the current repository
-    $secretsApiUrl = "$baseApiUrl/$($_.owner.login)/$($_.name)/actions/secrets"
-    # Define the API URL for the environments of the current repository
-    $environmentsApiUrl = "$baseApiUrl/$($_.owner.login)/$($_.name)/environments"
-    # Define the API URL for the collaborators of the current repository
-    $collaboratorsApiUrl = "$baseApiUrl/$($_.owner.login)/$($_.name)/collaborators"
+    $baseApiUrl = "https://api.github.com/repos"
 
-    # Initialize the hasActions variable to False
-    $hasActions = $false
-    # Initialize the hasSecrets variable to False
-    $hasSecrets = $false
-    # Initialize the environments variable to an empty array
-    $environments = @()
+    # The list of repositories is in the response
+    $repositories = $response | ForEach-Object {
+        Write-Host "Processing repository $($_.name)..."
+        # Define the API URL for the workflows directory of the current repository
+        $apiUrl = "$baseApiUrl/$($_.owner.login)/$($_.name)/contents/.github/workflows"
+        # Define the API URL for the secrets of the current repository
+        $secretsApiUrl = "$baseApiUrl/$($_.owner.login)/$($_.name)/actions/secrets"
+        # Define the API URL for the environments of the current repository
+        $environmentsApiUrl = "$baseApiUrl/$($_.owner.login)/$($_.name)/environments"
+        # Define the API URL for the collaborators of the current repository
+        $collaboratorsApiUrl = "$baseApiUrl/$($_.owner.login)/$($_.name)/collaborators"
 
-    # Make the API request and save the response in a variable
-    try {
-        Write-Host "Starting API request for workflows in repository $($_.name)..."
+        # Initialize the hasActions variable to False
+        $hasActions = $false
+        # Initialize the hasSecrets variable to False
+        $hasSecrets = $false
+        # Initialize the environments variable to an empty array
+        $environments = @()
+
+        # Make the API request and save the response in a variable
         try {
-            $workflowResponse = Invoke-RestMethod -Uri $apiUrl -Headers $headers -Method Get -ErrorAction Stop
+            Write-Host "Starting API request for workflows in repository $($_.name)..."
+            try {
+                $workflowResponse = Invoke-RestMethod -Uri $apiUrl -Headers $headers -Method Get -ErrorAction Stop
+            }
+            catch {
+                Write-Host "No workflows found in repository $($_.name). Skipping..."
+                $workflowResponse = @()
+            }
+
+            # Check if the response contains any workflows
+            if ($workflowResponse.Count -eq 0) {
+                Write-Host "No workflows found in repository $($_.name). Skipping..."
+            }
+
+            $hasActions = $true
+            Write-Host "Finished API request for workflows. Repositories checked: $($_.name)"
         }
         catch {
-            Write-Host "No workflows found in repository $($_.name). Skipping..."
-            $workflowResponse = @()
-        }
-
-        # Check if the response contains any workflows
-        if ($workflowResponse.Count -eq 0) {
+            # An error occurred, so leave hasActions as False
             Write-Host "No workflows found in repository $($_.name). Skipping..."
         }
 
-        $hasActions = $true
-        Write-Host "Finished API request for workflows. Repositories checked: $($_.name)"
-    }
-    catch {
-        # An error occurred, so leave hasActions as False
-        Write-Host "No workflows found in repository $($_.name). Skipping..."
-    }
+        # Make the API request and save the response in a variable
+        try {
+            Write-Host "Starting API request for Secrets in repository $($_.name)..."
+            $secretsResponse = Invoke-RestMethod -Uri $secretsApiUrl -Headers $headers -Method Get -ErrorAction Stop
 
-    # Make the API request and save the response in a variable
-    try {
-        Write-Host "Starting API request for Secrets in repository $($_.name)..."
-        $secretsResponse = Invoke-RestMethod -Uri $secretsApiUrl -Headers $headers -Method Get -ErrorAction Stop
-
-        # Check if the response contains any secrets
-        $hasSecrets = $secretsResponse.secrets.Count -gt 0
-    }
-    catch {
-        # An error occurred, so leave hasSecrets as False
-        Write-Host "An error occurred: $_"
-    }
-
-    try {
-        Write-Host "Starting API request for Environments in repository $($_.name)..."
-        $environmentsResponse = Invoke-RestMethod -Uri $environmentsApiUrl -Headers $headers -Method Get -ErrorAction Stop
-
-        # Check if the response contains any environments
-        if ($environmentsResponse.environments.Count -gt 0) {
-            $environments = $environmentsResponse.environments | ForEach-Object { $_.name }
-            $environments = $environments -join ', '
+            # Check if the response contains any secrets
+            $hasSecrets = $secretsResponse.secrets.Count -gt 0
         }
-    }
-    catch {
-        Write-Host "An error occurred: $_"
-    }
+        catch {
+            # An error occurred, so leave hasSecrets as False
+            Write-Host "An error occurred: $_"
+        }
 
-    # Initialize the userPermissions variable to an empty array
-    $userPermissions = @()
+        try {
+            Write-Host "Starting API request for Environments in repository $($_.name)..."
+            $environmentsResponse = Invoke-RestMethod -Uri $environmentsApiUrl -Headers $headers -Method Get -ErrorAction Stop
 
-    # Make the API request and save the response in a variable
-    try {
-        Write-Host "Starting API request for Collaborators in repository $($_.name)..."
-        $collaboratorsResponse = Invoke-RestMethod -Uri $collaboratorsApiUrl -Headers $headers -Method Get -ErrorAction Stop
-
-        # Check if the response contains any collaborators
-        if ($collaboratorsResponse.Count -gt 0) {
-            # Get the role for each collaborator
-            $userPermissions = $collaboratorsResponse | ForEach-Object {
-                $username = $_.login
-                $role = $_.role_name                
-                "${username}:${role}"
+            # Check if the response contains any environments
+            if ($environmentsResponse.environments.Count -gt 0) {
+                $environments = $environmentsResponse.environments | ForEach-Object { $_.name }
+                $environments = $environments -join ', '
             }
-            $userPermissions = $userPermissions -join ', '
+        }
+        catch {
+            Write-Host "An error occurred: $_"
+        }
+
+        # Initialize the userPermissions variable to an empty array
+        $userPermissions = @()
+
+        # Make the API request and save the response in a variable
+        try {
+            Write-Host "Starting API request for Collaborators in repository $($_.name)..."
+            $collaboratorsResponse = Invoke-RestMethod -Uri $collaboratorsApiUrl -Headers $headers -Method Get -ErrorAction Stop
+
+            # Check if the response contains any collaborators
+            if ($collaboratorsResponse.Count -gt 0) {
+                # Get the role for each collaborator
+                $userPermissions = $collaboratorsResponse | ForEach-Object {
+                    $username = $_.login
+                    $role = $_.role_name                
+                    "${username}:${role}"
+                }
+                $userPermissions = $userPermissions -join ', '
+            }
+        }
+        catch {
+            Write-Host "An error occurred: $_"
+        }
+
+        # Define the API URL for the teams of the current repository
+        $teamsApiUrl = "$baseApiUrl/$($_.owner.login)/$($_.name)/teams"
+
+        # Make the API request and save the response in a variable
+        $teamsResponse = Invoke-RestMethod -Uri $teamsApiUrl -Headers $headers -Method Get
+        # Create a string that contains the names of the teams and their permissions
+
+
+        # Create a string that contains the names of the teams and their permissions
+        Write-Host "Starting API request for Teams..."
+        # $teams = ($teamsResponse | ForEach-Object { "$($_.name):$($_.permission)" }) -join ', '
+
+        try {
+            # Make the API request and save the response in a variable
+            $teamsResponse = Invoke-RestMethod -Uri $teamsApiUrl -Headers $headers -Method Get
+        
+            # Create a string that contains the names of the teams and their permissions
+            Write-Host "Starting API request for Teams..."
+            $teams = ($teamsResponse | ForEach-Object { "$($_.name):$($_.permission)" }) -join ', '
+        }
+        catch {
+            Write-Host "Repository is empty or error: $($_.Exception.Message)"
+            $teams = "No teams or repository is empty"
+        }
+
+        # Select the desired properties and add the new properties
+        $_ | Select-Object name, full_name, id, node_id, size, visibility, created_at, updated_at, pushed_at, clone_url, has_issues, has_projects, has_downloads, has_wiki, has_pages, has_discussions, forks_count, mirror_url, archived, disabled, open_issues_count, license, allow_forking, is_template, web_commit_signoff_required, topics, forks, open_issues, watchers, default_branch, permissions,
+        @{Name = 'Advanced_Security'; Expression = { $_.security_and_analysis.advanced_security } },
+        @{Name = 'secret_scanning'; Expression = { $_.security_and_analysis.secret_scanning } },
+        @{Name = 'secret_scanning_push_protection'; Expression = { $_.security_and_analysis.secret_scanning_push_protection } },
+        @{Name = 'dependabot_security_updates'; Expression = { $_.security_and_analysis.dependabot_security_updates } },
+        @{Name = 'secret_scanning_validity_checks'; Expression = { $_.security_and_analysis.secret_scanning_validity_checks } },
+        @{Name = 'HasActions'; Expression = { $hasActions } },
+        @{Name = 'Teams'; Expression = { $teams } },
+        @{Name = 'HasSecrets'; Expression = { $hasSecrets } },
+        @{Name = 'Environments'; Expression = { $environments } },
+        @{Name = 'UserPermissions'; Expression = { $userPermissions } }
+    }
+
+    try {
+        # Get the Link header
+        $linkHeader = (Invoke-WebRequest -Uri $apiUrl -Headers $headers -Method Get).Headers.Link
+    
+        # Parse the Link header to get the URL for the next page
+        if ($linkHeader -match '<(?<nextUrl>.+?)>; rel="next"') {
+            $apiUrl = $Matches.nextUrl
+        }
+        else {
+            $apiUrl = $null
         }
     }
     catch {
-        Write-Host "An error occurred: $_"
+        Write-Host "Reached end of API Response(s), exiting..."
+        $apiUrl = $null
     }
-
-    # Define the API URL for the teams of the current repository
-    $teamsApiUrl = "$baseApiUrl/$($_.owner.login)/$($_.name)/teams"
-
-    # Make the API request and save the response in a variable
-    $teamsResponse = Invoke-RestMethod -Uri $teamsApiUrl -Headers $headers -Method Get
-    # Create a string that contains the names of the teams and their permissions
-
-
-    # Create a string that contains the names of the teams and their permissions
-    Write-Host "Starting API request for Teams..."
-    $teams = ($teamsResponse | ForEach-Object { "$($_.name):$($_.permission)" }) -join ', '
-
-    # Select the desired properties and add the new properties
-    $_ | Select-Object name, full_name, id, node_id, size, visibility, created_at, updated_at, pushed_at, clone_url, has_issues, has_projects, has_downloads, has_wiki, has_pages, has_discussions, forks_count, mirror_url, archived, disabled, open_issues_count, license, allow_forking, is_template, web_commit_signoff_required, topics, forks, open_issues, watchers, default_branch, permissions,
-    @{Name = 'Advanced_Security'; Expression = { $_.security_and_analysis.advanced_security } },
-    @{Name = 'secret_scanning'; Expression = { $_.security_and_analysis.secret_scanning } },
-    @{Name = 'secret_scanning_push_protection'; Expression = { $_.security_and_analysis.secret_scanning_push_protection } },
-    @{Name = 'dependabot_security_updates'; Expression = { $_.security_and_analysis.dependabot_security_updates } },
-    @{Name = 'secret_scanning_validity_checks'; Expression = { $_.security_and_analysis.secret_scanning_validity_checks } },
-    @{Name = 'HasActions'; Expression = { $hasActions } },
-    @{Name = 'Teams'; Expression = { $teams } },
-    @{Name = 'HasSecrets'; Expression = { $hasSecrets } },
-    @{Name = 'Environments'; Expression = { $environments } },
-    @{Name = 'UserPermissions'; Expression = { $userPermissions } }
-}
+} while ($apiUrl)
 
 # Define the path to the CSV file in the Documents folder
 $csvPath = "$([Environment]::GetFolderPath('MyDocuments'))\github_${organization}_output.csv"
 
 # Check if the CSV file already exists
+Write-Host "Checking for existence of previous CSV file..."
 if (Test-Path $csvPath) {
     # The CSV file exists, so delete it
+    Write-Host "Found file at $csvPath, deleting..."
     Remove-Item -Path $csvPath
+    Write-Host "Deleted file at $csvPath"
 }
 
 # Export the data to the new CSV file
+Write-Host "Exporting Data to CSV File at $csvPath..."
 $repositories | Export-Csv -Path $csvPath -NoTypeInformation
